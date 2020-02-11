62ee3d50b110e94616e30d92a0176b755daad693
Dan Zhao <dan_zhao@berkeley.edu>
2020-02-11 10:43:33 -0800

    test

--- batch_processing/util.py
+++ batch_processing/util.py
@@ -2,16 +2,12 @@ import pyspark
 from pyspark.sql import SparkSession
 
 def create_spark_session(app_name):
-	"""
-	Create a new spark session with the APP_NAME.
-	"""
+	# Create a new spark session with the APP_NAME.
 	spark = SparkSession.builder.appName(app_name).getOrCreate()
-    return spark
+	return spark
 
 def create_df_from_csv_paths(spark, paths, schema = None):
-	"""
-	Create a dataframe from csv paths. 
-	"""
+	# Create a dataframe from csv paths. 
 	if schema:
 		df = spark.read.format("csv").option("header", "true").\
         	schema(schema).\
