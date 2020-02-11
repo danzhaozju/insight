fd7c94c9877482d148bcda3e1b4ad6e05baa1acc
Dan Zhao <dan_zhao@berkeley.edu>
2020-02-11 14:51:49 -0800

    green_preprocess

--- batch_processing/preprocess.py
+++ batch_processing/preprocess.py
@@ -4,8 +4,6 @@ from pyspark.sql.functions import *
 from util import create_spark_session, create_df_from_csv_paths, generate_paths
 from schema import BIKE_SCHEMA, YELLOW_TAXI_SCHEMA_201308_201412, YELLOW_TAXI_SCHEMA_201501_201606, YELLOW_TAXI_SCHEMA_201607_201906, GREEN_TAXI_SCHEMA_201308_201412, GREEN_TAXI_SCHEMA_201501_201606, GREEN_TAXI_SCHEMA_201607_201812, GREEN_TAXI_SCHEMA_201901_201906
 
-findspark.init("/usr/local/spark")
-
 def preprocess_bike(spark):
 	"""
 	The funcion reads in all the .csv files of New York City citibike as dataframes with 
@@ -23,12 +21,12 @@ def preprocess_bike(spark):
 def preprocess_yellow_taxi(spark):
 	head = 's3a://ny-taxi-trip-data/yellow_taxi/yellow_tripdata_'
 	tail = '.csv'
-	# yellow_taxi_paths = generate_paths(head, tail,'2013-08-01','2014-12-01','%Y-%m')
-	# trips = create_df_from_csv_paths(spark, yellow_taxi_paths, YELLOW_TAXI_SCHEMA_201308_201412)
-	# preprocessed_trips = trips.select(['start_time','end_time','start_longitude','start_latitude',
-	# 	'end_longitude','end_latitude','passenger_count','distance','total_amount'])
-	# target_path = 's3a://ny-taxi-trip-data/yellow_taxi/parquet/preprocessed-yellow-taxi-201308_201412'
-	# preprocessed_trips.write.mode("overwrite").parquet(target_path)
+	yellow_taxi_paths = generate_paths(head, tail,'2013-08-01','2014-12-01','%Y-%m')
+	trips = create_df_from_csv_paths(spark, yellow_taxi_paths, YELLOW_TAXI_SCHEMA_201308_201412)
+	preprocessed_trips = trips.select(['start_time','end_time','start_longitude','start_latitude',
+		'end_longitude','end_latitude','passenger_count','distance','total_amount'])
+	target_path = 's3a://ny-taxi-trip-data/yellow_taxi/parquet/preprocessed-yellow-taxi-201308_201412'
+	preprocessed_trips.write.mode("overwrite").parquet(target_path)
 
 	yellow_taxi_paths = generate_paths(head, tail,'2015-01-01','2016-06-01','%Y-%m')
 	trips = create_df_from_csv_paths(spark, yellow_taxi_paths, YELLOW_TAXI_SCHEMA_201501_201606)
@@ -37,17 +35,17 @@ def preprocess_yellow_taxi(spark):
 	target_path = 's3a://ny-taxi-trip-data/yellow_taxi/parquet/preprocessed-yellow-taxi-201501_201606'
 	preprocessed_trips.write.mode("overwrite").parquet(target_path)
 
-	# yellow_taxi_paths = generate_paths(head, tail,'2016-07-01','2019-06-01','%Y-%m')
-	# trips = create_df_from_csv_paths(spark, yellow_taxi_paths, YELLOW_TAXI_SCHEMA_201607_201906)
-	# preprocessed_trips = trips.select(['start_time','end_time','start_locationID','end_locationID',
-	# 	'passenger_count','distance','total_amount'])
-	# preprocessed_trips.createOrReplaceTempView("taxi")
-	# taxi_lat_lon = spark.sql("SELECT start_time,end_time,P.longitude AS start_longitude,P.latitude AS start_latitude,D.longitude AS end_longitude,D.latitude AS end_latitude,passenger_count,distance,total_amount\
-	# 	FROM taxi,loc_id AS P,loc_id AS D\
-	# 	WHERE start_locationID = P.location_i\
-	# 	AND end_locationID = D.location_i")
-	# target_path = 's3a://ny-taxi-trip-data/yellow_taxi/parquet/preprocessed-yellow-taxi-201607_201906'
-	# taxi_lat_lon.write.mode("overwrite").parquet(target_path)
+	yellow_taxi_paths = generate_paths(head, tail,'2016-07-01','2019-06-01','%Y-%m')
+	trips = create_df_from_csv_paths(spark, yellow_taxi_paths, YELLOW_TAXI_SCHEMA_201607_201906)
+	preprocessed_trips = trips.select(['start_time','end_time','start_locationID','end_locationID',
+		'passenger_count','distance','total_amount'])
+	preprocessed_trips.createOrReplaceTempView("taxi")
+	taxi_lat_lon = spark.sql("SELECT start_time,end_time,P.longitude AS start_longitude,P.latitude AS start_latitude,D.longitude AS end_longitude,D.latitude AS end_latitude,passenger_count,distance,total_amount\
+		FROM taxi,loc_id AS P,loc_id AS D\
+		WHERE start_locationID = P.location_i\
+		AND end_locationID = D.location_i")
+	target_path = 's3a://ny-taxi-trip-data/yellow_taxi/parquet/preprocessed-yellow-taxi-201607_201906'
+	taxi_lat_lon.write.mode("overwrite").parquet(target_path)
 
 def preprocess_green_taxi(spark):
 	head = 's3a://ny-taxi-trip-data/green_taxi/green_tripdata_'
@@ -92,14 +90,15 @@ def preprocess_green_taxi(spark):
 
 
 if __name__ == '__main__':
-	spark = create_spark_session('preprocess_trips_data')
+	findspark.init("/usr/local/spark")
+	spark = create_spark_session('preprocess_green_trips_data')
 
 	taxi_locID_path = 's3a://ny-taxi-trip-data/taxi_locID_lon_lat.csv'
 	taxi_locIDs = create_df_from_csv_paths(spark, taxi_locID_path)
 	taxi_locIDs.createOrReplaceTempView("loc_id")
 
 	# preprocess_bike(spark)
-	preprocess_yellow_taxi(spark)
+	# preprocess_yellow_taxi(spark)
 	preprocess_green_taxi(spark)
 
 
--- batch_processing/util.py
+++ batch_processing/util.py
@@ -2,13 +2,18 @@ import pyspark
 import pandas as pd
 from pyspark.sql import SparkSession
 
-# Create a new spark session with the APP_NAME.
+
 def create_spark_session(app_name):
+    """
+    Create a new spark session with the APP_NAME.
+    """
     spark = SparkSession.builder.appName(app_name).getOrCreate()
     return spark
 
-# Create a dataframe from csv paths.
 def create_df_from_csv_paths(spark, paths, schema = None):
+    """
+    Create a dataframe from csv paths.
+    """
     if schema:
         df = spark.read.format("csv")\
             .options(header='true') \
@@ -23,8 +28,10 @@ def create_df_from_csv_paths(spark, paths, schema = None):
             load(paths)
     return df
 
-# Generate path monthly with output_format between time1 and time2
 def generate_paths(head, tail, time1, time2, output_format):
+    """
+    Generate path monthly with output_format between time1 and time2
+    """
     months = pd.date_range(time1,time2, 
               freq='MS').strftime(output_format).tolist()
     paths = [head + month + tail for month in months]
