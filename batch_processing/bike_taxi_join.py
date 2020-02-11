e484aa2e4b60500075018a72e14405c65e68cf61
Dan Zhao <dan_zhao@berkeley.edu>
2020-02-11 15:46:51 -0800

    Modify bike_taxi_join.py

--- batch_processing/bike_taxi_join.py
+++ batch_processing/bike_taxi_join.py
@@ -7,4 +7,6 @@ if __name__ == '__main__':
 	findspark.init("/usr/local/spark")
 	spark = create_spark_session('join_taxi_bike')
 
-	# df = spark.read.parquet()
+	path = 's3a://citi-bike-trip-data/parquet/preprocessed-citi-bike-trips-201308_201906'
+	bike_trips = spark.read.parquet(path)
+	bike_trips.show()
